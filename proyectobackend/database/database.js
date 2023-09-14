2390fc9d47fe4277d7dee4c060cfe63f03722a98
Yoselyn Díaz <126622819+yosediaz@users.noreply.github.com>
2023-09-14 20:22:41 -0300

    Update database.js

--- proyectobackend/database/database.js
+++ proyectobackend/database/database.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors(
   {
-    origin: ["https://deploy-merm-lwhp.vercel.app"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true 
 
